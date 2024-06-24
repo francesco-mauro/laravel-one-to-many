01bfcec069ef788e791fad771594e48f7835da1a
francesco-mauro <francesco.mauro180@gmail.com>
2024-06-24 20:36:18 +0200

    feat: defined relation OtM, MtM

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,8 +4,16 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Type;
 
 class Post extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['title', 'description', 'slug', 'type_id'];
+
+    public function type()
+    {
+        return $this->belongsTo(Type::class);
+    }
 }
